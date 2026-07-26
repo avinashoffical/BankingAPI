0b68afde6cfe71400de2bbe509126b6ef5a8afd4
Avinash <avinashoffical@gmail.com>
2026-07-26 16:55:23 +0530

    created user entity

--- src/main/java/com/avinash/BankingAPI/entity/User.java
+++ src/main/java/com/avinash/BankingAPI/entity/User.java
@@ -1,4 +1,30 @@
 package com.avinash.BankingAPI.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String role;
+
+    private LocalDateTime createdAt;
+
 }
